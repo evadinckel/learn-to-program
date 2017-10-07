7cbbd1f0ea4252f01489014844a8265c16b64935
Eva Dinckel <evadinckel@gmail.com>
2017-10-07 18:42:19 +0100

    commit to chapter6 of CP's book

---
+++ chapter6/chapter6_book_practice.rb
@@ -0,0 +1,79 @@
+#puts ('hello '. + 'world')
+#puts ((10. * 9). + 9)
+
+#puts self
+
+#string methods: .reverse, .length
+
+#var1 = 'stop'
+#var2 = 'deliver repaid desserts'
+#var3 = '.... TCELES B HSUP A magic spell?'
+
+
+#puts var1.reverse
+#puts var2.reverse
+# var3.reverse
+
+#puts var1
+#puts var2
+#puts var3
+
+
+#puts 'What is your full name?'
+#name = gets.chomp
+#puts 'Did you know that there are ' + name.length.to_s + ' characters in your name, ' + name + '?'
+
+#practice page 30
+#puts 'What is your first name?'
+#first_name = gets.chomp
+#puts 'And what is your last name?'
+#last_name = gets.chomp
+
+#puts 'Did you know that there were ' + first_name.length.to_s + ' letters in your first name?'
+#puts 'And ' + last_name.length.to_s + ' letters in your last name?'
+# to be changed: puts 'There are ' + first_name.length + last_name.length + ' letters in total in your name.'
+
+#letters = 'aAaaBBBBccCCcDDeeEE'
+#puts letters.upcase
+#puts letters.downcase
+#puts letters.swapcase
+#puts letters.capitalize
+#puts 'a'.capitalize
+#puts letters
+
+#line_width  = 50
+#puts(           'nursery rhyme line 1'.center(line_width))
+#puts(  'nursery rhyme line 2'.center(line_width))
+#puts(                 'nursery rhyme line 3'.center(line_width))
+
+
+
+
+#puts 5**2
+#puts 5**0.5
+#puts 7/3
+#puts 7%3
+#puts 365%7
+
+
+#puts (5-2).abs
+#puts (2-3).abs
+
+
+
+#puts rand
+#puts rand
+#puts (rand(100))
+#puts (rand(100))
+#puts (rand(1))
+#puts 'The weather forecast says there is a'
+#puts (rand(101).to_s + '% chance of rain tomorrow. ')
+#puts 'But you can never really trust the forecast.'
+
+
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1 + Math.sqrt(5))/2)
---
+++ chapter6/chapter6_exercise1.rb
@@ -0,0 +1,3 @@
+puts 'What is it, why are you bothering me? I am really busy!'
+request = gets.chomp
+puts 'Whaddya mean '.upcase + request.upcase + ' ??!! You\'re fired!'.upcase
