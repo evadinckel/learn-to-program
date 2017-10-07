a503f4f8edc62ea749417777bbab3ac97d1d5322
Eva Dinckel <evadinckel@gmail.com>
2017-10-07 15:55:45 +0100

    commit on ex1/chapter 5 of CP's book

---
+++ chapter5_exercise1.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, what is your first name?'
+first_name = gets.chomp
+puts 'And what is your middle name please?'
+middle_name = gets.chomp
+puts 'Finally, what is your last name?'
+last_name = gets.chomp
+puts 'Hello ' + first_name +' ' + middle_name + ' ' + last_name + ' ' +',nice to meet you!'
