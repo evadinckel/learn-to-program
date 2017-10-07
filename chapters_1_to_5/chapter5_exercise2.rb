c3bcd458d4c9ddbb5b97bbbb802472d25fdef965
Eva Dinckel <evadinckel@gmail.com>
2017-10-07 18:55:23 +0100

    commit of ch1 and 2 of CP's book

--- chapters_1_to_5/chapter5_exercise2.rb
+++
@@ -1,7 +0,0 @@
-puts 'What is your favourite number?'
-favourite_number = gets.chomp.to_i
-suggested_number = favourite_number + 1
-puts 'Cool, cool...How about ' + suggested_number.to_s + ' ,don\'t you think it\'s even better?'
-
-
-#Quick note here: convert the number back to a string: .to_s
